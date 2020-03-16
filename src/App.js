efc29bf45b0c2934daa5933b71d40daddf73f5b9
Lisa.dev <j0496401@gmail.com>
2020-03-16 23:25:15 +0900

    <로그인/로그아웃 구현> 인증 모듈을 이용해서 로그인과 로그아웃 기능을 앱의 최상위 컴포넌트인 <App/>에 추가
    로그인된 사용자 정보는 user state로 관리하고,
    authenticated 변수에 로그인된 사용자가 존재하는지, 즉 인증 여부를 저장합니다.
    
    그리고 login 함수는 인증 모듈을 통해 로그인한 사용자의 정보를 user state에 저장하고,
    logout 함수는 로그인한 사용자의 정보를 user state로 부터 지우도록 구현합니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link, Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
+import { signIn } from './auth';
 
 import Home from "./Home";
 import About from "./About";
@@ -7,6 +9,12 @@ import Profile from './Profile';
 import NotFound from "./NotFound";
 
 function App() {
+  const [user, setUser] = useState(null);
+  const authenticated = user != null;
+
+  const login = ({ email, password }) => setUser(signIn({ email, password }));
+  const logout = () => setUser(null);
+
   return (
     <Router>
       <header>
